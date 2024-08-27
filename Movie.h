b9e3a6388c789479bf9e1cee60c45fb930120998
gcaltair <143710288+gcaltair@users.noreply.github.com>
2024-08-27 10:45:12 +0800

    新增结构体

---
+++ Movie.h
@@ -0,0 +1,10 @@
+//
+// Created by G on 2024/8/27.
+//
+
+#ifndef MOVIE_C_MOVIE_H
+#define MOVIE_C_MOVIE_H
+typedef struct {
+
+}Movie;
+#endif //MOVIE_C_MOVIE_H
---
+++ Order.h
@@ -0,0 +1,10 @@
+//
+// Created by G on 2024/8/27.
+//
+
+#ifndef MOVIE_C_ORDER_H
+#define MOVIE_C_ORDER_H
+typedef struct {
+
+}Order;
+#endif //MOVIE_C_ORDER_H
