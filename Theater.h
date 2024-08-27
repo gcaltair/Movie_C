074688ab4cf721c45d6b81d7d3ea76755541e130
gcaltair <143710288+gcaltair@users.noreply.github.com>
2024-08-27 10:47:27 +0800

    新增结构体

---
+++ Theater.h
@@ -0,0 +1,12 @@
+//
+// Created by G on 2024/8/27.
+//
+
+#ifndef MOVIE_C_THEATER_H
+#define MOVIE_C_THEATER_H
+typedef struct {
+    char* theater_name;
+    int theater_capacity;
+    char* theater_type;
+};
+#endif //MOVIE_C_THEATER_H
