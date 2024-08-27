35c7c83a978419eed2a83ddbaa221304a8fe008e
gcaltair <143710288+gcaltair@users.noreply.github.com>
2024-08-27 20:04:11 +0800

    增加了admin,user和cinema的add函数和show函数

--- Vector.c
+++ Vector.c
@@ -9,19 +9,20 @@
 
 #define DEFAUT_CAPACITY 10
 #define EDGE_VALUE 1024
-Vector * vector_create() {
+Vector * vector_create(size_t elem_size) {
     Vector* v=calloc(1,sizeof(Vector));
     if(v==NULL) {
-        printf("calloc error");
-        return NULL;
+        perror("Fail to initialize vector!");
+        exit(EXIT_FAILURE);
     }
-    v->data=calloc(DEFAUT_CAPACITY,sizeof(ElementType));
+    v->data=calloc(DEFAUT_CAPACITY,elem_size);
     if(v->data==NULL) {
-        printf("malloc error");
+        perror("Fail to initialize data!");
         free(v);
         return NULL;
     }
     v->capacity=DEFAUT_CAPACITY;
+    v->size=0;
     return v;
 
 }
@@ -32,16 +33,17 @@ void vector_destroy(Vector *v) {
 }
 
 static void vector_resize(Vector *v) {
-    int old_capacity=v->capacity;
-    int new_capacity=old_capacity>EDGE_VALUE?((old_capacity>>1)+old_capacity):(old_capacity<<1);
-    int* p_=realloc(v->data,sizeof(int)*new_capacity);
+    size_t old_capacity=v->capacity;
+    size_t new_capacity=old_capacity>EDGE_VALUE?((old_capacity>>1)+old_capacity):(old_capacity<<1);
+    void* p_=realloc(v->data,(v->elem_size)*new_capacity);
     if(p_==NULL) {
         printf("realloc error");
         exit(-1);
     }
     v->data=p_;
 }
-void vector_push_back(Vector *v, ElementType element) {
+void vector_push_back(Vector *v,void* value) {
     if(v->size>=v->capacity-1) vector_resize(v);
-    v->data[++(v->size)]=element;
+    v->data[++(v->size)]=value;
 }
+
