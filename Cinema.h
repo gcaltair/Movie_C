8f0bbc8816dc50ad8da3a5f3cf8b7979e63c6624
gcaltair <143710288+gcaltair@users.noreply.github.com>
2024-08-28 10:56:45 +0800

    Cinema.h增加链表

--- Cinema.h
+++ Cinema.h
@@ -4,11 +4,17 @@
 
 #ifndef MOVIE_C_CINEMA_H
 #define MOVIE_C_CINEMA_H
-typedef struct {
+typedef struct Cinema{
     char* cinema_name;
     char* location;
     int cinema_id;
+    struct Cinema* next;
 }Cinema;
-Cinema* cinema_add(const char* name, const char* location, int id);
+Cinema* cinema_create_list(const char* name, const char* location, int id,Cinema* next);
+void cinema_add_to_list(Cinema** head, const char* name, const char* location, int id,Cinema* next);
+Cinema* cinema_find_by_id(Cinema* head, int id);
+Cinema* cinema_add(const char* name, const char* location, int id,Cinema* next);
 void cinema_show(const Cinema* cinema);
+void cinema_show_all(Cinema* head);
+
 #endif //MOVIE_C_CINEMA_H
