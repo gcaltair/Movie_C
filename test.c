7c8ad3b5cb1543db6018b11fa2f8b18a8e4b1495
旷江舟 野 <eurojiang08@163.com>
2024-08-29 16:12:20 +0800

    jyz

--- test.c
+++ test.c
@@ -5,22 +5,17 @@
 #include "User.h"
 #include "admin.h"
 #include "Cinema.h"
+#include"Movie.h"
 #include "Theater.h"
-
+#include "Order.h"
 int main(){
 
-//    Cinema* t = cinema_create("Cinema A", "Location A", 1);
-//    Cinema *cinema_list= cinema_create_list(t);;
-//    cinema_direct_add_to_list(&cinema_list, "Cinema C", "Location C", 3);
-//    Theater *theater_temp= theater_create("theater1",20,cinema_list,"3D");
-//    Theater *theater_list=theater_create_list(theater_temp);
-//    theater_add_to_list(&theater_list,theater_temp);
-//    cinema_show(theater_temp->cinema);
-//    theater_show(cinema_list->theater);
-    User* head= user_create("114514","gc","man","114514","114514","114514@homo.com",0.0);
-    User* temp=user_create("123456","cg","unknow","22222","114514","114514@homo.com",0.0);
-    user_add_to_list(&head,temp);
-    user_show_all(head);
-    user_show(user_find_by_id(head,"114514"));
+	Order* head = NULL;
+	Cinema* cin = cinema_create("A", "location 1", 1);
+	User* usr = user_create("111", "2323", "dsds", "111", "asdf", "dadsa", 0.1);
+	Theater* tha = theater_create("sadsd", 10, "A", "3D");
+	Movie* mov=movie_create("12212", tha, "500-11:10", "500-13:30", 100, 50.0, 1, "3D");
+	add_order(usr, mov, head);
+	
 
 }
